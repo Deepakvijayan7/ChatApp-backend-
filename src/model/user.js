28f7c3cb60ce26b9f6ab541439aaf032db0d8e13
Deepakvijayan07 <deepakvijayan31@gmail.com>
2022-12-12 13:02:12 +0530

    first commit

---
+++ src/model/user.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+var UserSchema=new Schema({
+    Username:String,
+    Firstname:String,
+    Lastname:String,
+    Email:String,
+    Password:String,
+    Blocked_users:[String],
+    Blocked_By:[String],
+    privateRooms:[String],
+    onlineStatus:String,
+    newNotification:[String]
+})
+
+var UserData=mongoose.model('Users',UserSchema);
+
+module.exports=UserData;
